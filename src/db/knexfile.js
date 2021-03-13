7dea5b1f91e7e1bf612c1e761f4ccf1fdf2c8748
Alfred Kirakosian <gituser@tangosend.com>
2021-03-13 12:56:59 -0800

    Add procfile and update database config

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -1,32 +1,25 @@
-const path = require('path');
+const path = require("path");
 
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: path.join(__dirname, 'db.sqlite')
+      filename: path.join(__dirname, "db.sqlite"),
     },
     migrations: {
-      directory: path.join(__dirname, "migrations")
+      directory: path.join(__dirname, "migrations"),
     },
     useNullAsDefault: true,
-    asyncStackTraces: true
+    asyncStackTraces: true,
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user:     process.env.DB_USER,
-      password: process.env.DB_PASS
-    },
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
     migrations: {
-      directory: path.join(__dirname, "migrations")
-    }
-  }
-
+      directory: path.join(__dirname, "migrations"),
+    },
+  },
 };
